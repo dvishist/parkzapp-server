3a070c9b19d53be30b5657d1decc5a9b0defebf2
dvishist <54056185+dvishist@users.noreply.github.com>
2020-09-23 19:39:44 +1000

    add findParking by distance using geolib

--- src/routers/parking.js
+++ src/routers/parking.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = new express.Router()
 const Parking = require('../db/models/parking')
 const auth = require('../middleware/auth')
+const geolib = require('geolib')
+
 
 //create new Parking
 router.post('/parkings', auth, async (req, res) => {
@@ -11,6 +13,43 @@ router.post('/parkings', auth, async (req, res) => {
         res.status(201).send(parking)
     } catch (err) {
         res.status(400).send(err.message)
+    }
+})
+
+// //get Parkings by state
+// router.get('/parkings/find/:state', auth, async (req, res) => {
+//     try {
+//         const parkingList = Parking.find({ state: req.params.state })
+//         res.status(200).send(parkingList)
+//     } catch (err) {
+//         res.status(400).send(err)
+//     }
+// })
+
+//Get List of Parkings nearby
+//this will scale very poorly as it calculates the distance with all of the parking lots on database
+//should use mapbox to filter parkings by state or suburb
+router.post('/parkings/findNearby', auth, async (req, res) => {
+    try {
+        const user = req.body
+        const parkings = Parking.find({})
+        const result = parkings.map(parking => {
+            const distance = geolib.getDistance(
+                {
+                    latitude: user.latitude,
+                    longitude: user.longitude
+                },
+                {
+                    latitude: parking.coordinates.latitude,
+                    longitude: parking.coordinates.longitude
+                }
+                , 1)
+            return { parking, distance }
+        })
+        result = result.sort((item1, item2) => item1.distance - item2.distance)
+        res.status(200).send(result)
+    } catch (err) {
+        res.status(400).send(err)
     }
 })
 
