6a18811443877688d24319958b2ac737ac9c6b80
dvishist <54056185+dvishist@users.noreply.github.com>
2020-10-07 13:44:57 +1100

    remove ingress/egress routers from parking

--- src/routers/parking.js
+++ src/routers/parking.js
@@ -63,24 +63,6 @@ router.get('/parkings/:id', auth, async (req, res) => {
     res.send(parking)
 })
 
-// Ingress parking
-router.post('/parkings/:id/ingress', auth, async (req, res) => {
-    const parking = await Parking.findById(req.params.id)
-    if (!parking) throw new Error(`Couldn't find parking`)
-    parking.occupants++
-    await parking.save()
-    res.status(200).send(parking)
-})
-
-// Egress parking
-router.post('/parkings/:id/egress', auth, async (req, res) => {
-    const parking = await Parking.findById(req.params.id)
-    if (!parking) throw new Error(`Couldn't find parking`)
-    parking.occupants--
-    await parking.save()
-    res.status(200).send(parking)
-})
-
 router.delete('/parkings/:id', auth, async () => {
     try {
         await Parking.findByIdAndDelete(req.params.id)
